5d0a1228e4cfebe25df0e0bc9e21add2d2601ef2
Farkadi06 <58217000+Farkadi06@users.noreply.github.com>
2020-12-15 11:54:51 +0100

    Implimenting Parcelalable interface on the NoteInfo Class

--- app/src/main/java/com/example/notekeeper/NoteListActivity.java
+++ app/src/main/java/com/example/notekeeper/NoteListActivity.java
@@ -39,7 +39,7 @@ public class NoteListActivity extends AppCompatActivity {
     }
 
     private void initializeDisplayContent() {
-        ListView listNotes = findViewById(R.id.list_notes);
+        final ListView listNotes = findViewById(R.id.list_notes);
         List <NoteInfo> notes = DataManager.getInstance().getNotes();
         ArrayAdapter<NoteInfo> adapterNotes =
                     new ArrayAdapter<>(this,android.R.layout.simple_list_item_1,notes);
@@ -49,6 +49,9 @@ public class NoteListActivity extends AppCompatActivity {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long l) {
                 Intent intent =new Intent(NoteListActivity.this,NoteActivity.class);
+
+
+
                 startActivity(intent);
             }
         });
